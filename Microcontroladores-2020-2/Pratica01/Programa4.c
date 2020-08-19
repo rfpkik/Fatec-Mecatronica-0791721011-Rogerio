dd99a6009bf3f1d459acbe02dafc397f35556344
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-08-19 10:59:54 -0300

    Update Programa4.c

--- Microcontroladores-2020-2/Pratica01/Programa4.c
+++ Microcontroladores-2020-2/Pratica01/Programa4.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-float numero_real;
-int 
+  float numero_real;
+  int numero_inteiro;
+  char letra;
+  printf("Informe um valor real: ");
+  scanf("%f", &numero_real);
+  printf("Informe um valor inteiro: ");
+  scanf("%i", &numero_inteiro);
+  printf("Informe uma letra: ");
+  scanf("%c", &letra);
+  
+  printf("Valor real: %f\n", numero_real);
+  printf("Valor inteiro: %i\n", numero_inteiro);
+  printf("Letra: %c\n", letra);
+  printf("Palmeiras não tem mundialn");
+  
   return 0;
 }
