618f5bc54e9ab52c7ad615de1eef7dc3670db247
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-08-27 10:27:55 -0300

    Create calculo.py

---
+++ LTP2-2020-2/Pratica02/calculo.py
@@ -0,0 +1,5 @@
+numero = int(input("Informe um valor: ")) #variavel, informar que é numero, solicitando um numero
+
+resposta = numero / 2 # variavel, recebe variavel e faz a conta
+
+print(resposta) # exibe a resposta na tela
