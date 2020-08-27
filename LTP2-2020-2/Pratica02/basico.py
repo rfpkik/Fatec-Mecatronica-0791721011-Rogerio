1e2add339699402448672824d2a0c2bb19c3ed47
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-08-27 10:16:53 -0300

    Create basico.py

---
+++ LTP2-2020-2/Pratica02/basico.py
@@ -0,0 +1,9 @@
+print("Ola mundo")#pode ser aspas duplas
+print('Bom dia') #pode ser aspas simples
+
+nome = input("Digite seu nome: ")# variavel nome recebe input, que recebe entrada
+
+print("Bom dia", nome, "!") #exibe resposta usando o texto do usuario
+'''
+Isto é um bloco de comentario
+'''
