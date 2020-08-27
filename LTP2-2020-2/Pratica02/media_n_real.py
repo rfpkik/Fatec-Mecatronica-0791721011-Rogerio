c78a1f73989e63da87fcf28488e7650c4b96c50c
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-08-27 10:39:50 -0300

    Create media_n_real.py

---
+++ LTP2-2020-2/Pratica02/media_n_real.py
@@ -0,0 +1,6 @@
+#usuario fala 2 numeros e nós calculamos a media deles
+numero1 = float(input("Numero 1: "))
+numero2 = float(input("Numero 2: "))
+media = (numero1 + numero2) / 2
+# o \n faz pular linha quando ele esta no meio da string
+print("A média é:\n", media)
