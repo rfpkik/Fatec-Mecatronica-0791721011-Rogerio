2ea6fac5f6b81f0db04c6f8e43cc61012af46e6c
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-08-27 10:33:58 -0300

    Create media.py

---
+++ LTP2-2020-2/Pratica02/media.py
@@ -0,0 +1,5 @@
+#usuario fala 2 numeros e nós calculamos a media deles
+numero1 = int(input("Numero 1: "))
+numero2 = int(input("Numero 2: "))
+media = (numero1 + numero2) / 2
+print("A média é:", media)
