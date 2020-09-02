2cd006b40c8e71fd4fa0b90d24bc9ee836c9d551
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-09-02 10:10:19 -0300

    Create endereco.c

---
+++ Microcontroladores-2020-2/Pratica03/endereco.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(void){
+int minhaVariavel;
+minhaVariavel = 10;
+printf("Valor da minhaVariavel: %i\n", minhaVariavel);
+printf("Valor da minhaVariavel: %i\n", &minhaVariavel);
+return 0;
+}
