5b88731dc2cfd89de4bffae7eeb207a6cd003f28
rfpkik <69902554+rfpkik@users.noreply.github.com>
2020-09-02 09:13:29 -0300

    Create converterParaRadianos.c

---
+++ Microcontroladores-2020-2/Pratica03/converterParaRadianos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+float converterParaRadiano(float graus){
+  float resposta;
+  resposta = graus * M_PI / 180;
+  return resposta;
+
+}
+
+int main(void){
+  float angulo;
+  printf("Informe o valor do angulo ");
+  scanf("%f", &angulo);
+  printf("O Valor de %f em radianos eh %f\n", angulo, converterParaRadiano(angulo));
+
+  return 0;
+}
